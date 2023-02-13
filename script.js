8e91090a40b04a050bd954ae35ad696b43d4c262
Agustin <agtsaavedra@gmail.com>
2023-02-13 15:17:44 -0300

    Cambiado idea de programa, ahora es una app de clima utilizando objetos ciudades

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
 
-
-function calculoDivisa() {
-
-    //Ingresa y solicita nombre
-    let name = prompt("Ingrese su nombre");
+function menuPrincipal () {
     let endProgram;
+    let name = prompt("BIENVENIDO APP Clima! Ingrese su nombre");
     
 
-    //Se ejecuta siempre que endProgram sea Si o distinto de No
-
+    
     while(endProgram != "No"){
-        let moneda = prompt(`Hola ${name}, Indique moneda \n Euro, Dolar, Real`);
-        while (moneda != "Euro" && moneda != "Dolar" && moneda != "Real")
-            {
-            moneda = prompt ("Debe ingresar una moneda valida \n Euro, Dolar, Real");   
-            }
+       
+        let ciudadPrompt = prompt(`Hola ${name}, Indique el numero segun la ciudad \n 
+        1-Mar del Plata,\n
+        2-Buenos Aires \n 
+        3-Rosario \n 
+        4-Bahia Blanca`);  
     
-            let valor = prompt("Ingrese un valor que quiera convertir (PESOS)")
-            while(isNaN(valor)){
-                valor = prompt("Por favor ingrese un numero valido");
-            }  
-            //Llama a funcion que realiza la conversion
-        operacionDivisa(valor, moneda);
-        endProgram = prompt(`Desea hacer otra conversion:\n Si / No`);
-            while (endProgram != 'Si' && endProgram != 'No') {
-        endProgram = prompt(`Desea hacer otra conversion:\n Si / No`);
-            }
-    }  
-    
-}
-//Funcion que se encarga de multiplicar el valor ingresado por el valor actual de la moneda (recibe dos parametros tipo de moneda y el valor)
-function operacionDivisa (valor, moneda){
-    switch (moneda){
-        case "Euro": alert("La cantidad de $"+ valor +"  equivale a EUROS: $ " + valor*207,80);
-        break;
-        case "Dolar": alert("La cantidad de $"+ valor +"  equivale a  DOLARES:$ "+ valor*201,59);
-        break;
-        case "Real": alert("La cantidad de $"+ valor +"  equivale a  REALES: $ " + valor*36,29);
-        break;
+        while (ciudadPrompt != "1" && ciudadPrompt != "2" && ciudadPrompt != "3" && ciudadPrompt != "4"){
+            ciudadPrompt = prompt (`Por favor ${name}, Indique un numero de ciudad valido \n 
+            1-Mar del Plata,\n
+            2-Buenos Aires \n 
+            3-Rosario \n 
+            4-Bahia Blanca`);
+        }
+        calculoTemperatura(ciudadPrompt);
+
+        
+
+        while (endProgram != 'Si' && endProgram != 'No') {
+        endProgram = prompt(`Desea hacer otra consulta:\n Si / No`);
+        } 
     }
 
+    
+
+   
 }
-calculoDivisa();
+
+
+function calculoTemperatura (ciudadPrompt){
+
+    //Defincion de objeto ciudades
+    function city(ciudad, temperatura, viento, presion) {
+        this.ciudad = ciudad;
+        this.temperatura = temperatura;
+        this.viento = viento;
+        this.presion = presion;
+      }
+      //Creacion de objetos con distintas propiedades segun ciudades
+    var mdq = new city ("Mar del Plata", "27 Grados Celcius", "35 KM/H", "1003 hPa");
+    var bsas = new city ("Buenos Aires", "34 Grados Celcius", "21 KM/H", "1123 hPa");
+    var ros = new city ("Rosario", "23 Grados Celcius", "11 KM/H", "1220 hPa");
+    var bb = new city ("Bahia Blanca", "19 Grados Celcius", "54 KM/H", "1020 hPa");  
+
+    const arrayCitys = (mdq, bsas, ros, bb);
+    //Muestra de objeto con alert
+
+    switch (ciudadPrompt){
+        case "1": alert("La temperatura en " + mdq.ciudad + " es de " + mdq.temperatura + "\n el viento es de " + mdq.viento + "\n y la presion es de " +mdq.presion);
+        break;
+        case "2": alert("La temperatura en " + bsas.ciudad + " es de " + bsas.temperatura + "\n el viento es de " + bsas.viento + "\n y la presion es de "  +bsas.presion);
+        break;
+        case "3": alert("La temperatura en " + ros.ciudad + " es de " + ros.temperatura + "\n el viento es de " + ros.viento + "\n y la presion es de " +ros.presion);
+        break;
+        case "4": alert("La temperatura en " + bb.ciudad + " es de " + bb.temperatura + "\n el viento es de " + bb.viento + "\n y la presion es de " +bb.presion);
+        break;
+        
+    }
+
+}   
+
+menuPrincipal();
